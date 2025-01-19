17f81588b43fcb874435f83672d2dec9d0985479
RuhamaMalik <malikruhama7@gmail.com>
2025-01-19 14:55:39 +0500

    websocket and nodemailer

--- cron/app.js
+++ cron/app.js
@@ -1,13 +1,73 @@
+const http = require("http");
+const socketIo = require("socket.io");
 const express = require("express");
 const app = express();
-const cron = require("node-cron");
+const nodemailer = require("nodemailer");
+// Create an HTTP server
+const server = http.createServer();
 
-cron.schedule("*/10 * * * * *", function () {
-  console.log("running every 10 second");
+let connectedClients = [];
+
+const io = socketIo(server, {
+  cors: {
+    origin: "http://127.0.0.1:5501",
+  },
 });
-app.get("/", (req, res) => {
-  res.send("hello");
+
+io.use((socket, next) => {
+  const token = socket.handshake.headers.authorization;
+  if (token) {
+    next();
+  } else {
+    next(new Error("Socket authentication error"));
+  }
 });
-app.listen(3000, () => {
-  console.log("Server running at PORT");
+
+app.post("/", async (req, res) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "",
+        pass: "",
+      },
+    });
+    const info = await transporter.sendMail({
+      from: "", // sender address
+      to: ["", ""], // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world test?", // plain text body
+      html: `<h1 style = "color:red">Hello world tes?</h1>`, // html body
+    });
+    res.send("hello");
+  } catch (error) {
+    res.send("hello");
+
+    console.log(error, ">>>>");
+  }
+});
+
+// Handle client connection
+io.on("connection", (socket) => {
+  connectedClients.push(socket.id);
+  console.log("Client connected:", socket.id, connectedClients);
+
+  socket.on("chat", async ({ message }) => {
+    console.log("Received message:", message);
+    io.emit("chat", { message });
+  });
+
+  // Handle client disconnect
+  socket.on("disconnect", () => {
+    connectedClients = connectedClients.filter((id) => id !== socket.id);
+    console.log("Client disconnected:", socket.id, connectedClients);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Socket.IO server running on http://localhost:3000");
+});
+
+app.listen(4000, () => {
+  console.log("https server listening at http://localhost:4000");
 });
